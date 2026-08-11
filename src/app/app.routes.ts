8e188fe316b011efc3b5ccd254311915c8491a4d
unknown <bogdan.susman@icloud.com>
2026-08-11 15:46:12 +0300

    Added and connected the backend and database

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,8 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
 import { Home } from './home/home';
+import { ResetPassword } from './reset-password/reset-password';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -18,8 +20,13 @@ export const routes: Routes = [
     component: Register
   },
   {
-    path: 'home',
-    component: Home
+  path: 'home',
+  component: Home,
+  canActivate: [authGuard]
+},
+  {
+  path: 'reset-password',
+  component: ResetPassword
   },
   {
     path: '**',
---
+++ src/app/services/token.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+  private token: string | null = null;
+
+  setToken(token: string): void {
+    this.token = token;
+  }
+
+  getToken(): string | null {
+    return this.token;
+  }
+
+  clearToken(): void {
+    this.token = null;
+  }
+}
